d7d40352957f90b00f529856ddfa02372dbcf4ef
Ben Melcher <bmelcher@unomaha.edu>
2017-10-20 17:16:09 -0500

    Add files via upload

---
+++ temps/temps.ino
@@ -0,0 +1,68 @@
+#include "Adafruit_Si7021.h"
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+#include "Adafruit_SHT31.h"
+
+Adafruit_Si7021 sensor = Adafruit_Si7021();
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println("Adafruit MLX90614 test");
+
+  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
+
+  Serial.println("Si7021 test");
+  sensor.begin();
+
+  mlx.begin();
+
+}
+
+void loop() {
+
+  float t = sht31.readTemperature();
+  float h = sht31.readHumidity();
+
+  
+  Serial.println("MLX Test");
+  
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC()); 
+  Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
+  Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
+
+  Serial.println();
+
+  Serial.println("si7021");
+  Serial.print("Humidity:    "); Serial.print(sensor.readHumidity(), 2);
+  Serial.print("\tTemperature: "); Serial.println(sensor.readTemperature(), 2);
+  delay(100);
+
+  Serial.println();
+
+  Serial.println("SHT31");
+  if (! isnan(t)) {  // check if 'is not a number'
+    Serial.print("Temp *C = "); Serial.println(t);
+  } else { 
+    Serial.println("Failed to read temperature");
+  }
+  
+  if (! isnan(h)) {  // check if 'is not a number'
+    Serial.print("Hum. % = "); Serial.println(h);
+  } else { 
+    Serial.println("Failed to read humidity");
+  }
+  Serial.println();
+  delay(1000);
+
+  
+  // put your main code here, to run repeatedly:
+
+}
